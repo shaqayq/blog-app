c28047d5135b5cce2a8605538fb0d78b2ae99691
shaqayq <shaqayq.darwazi@gmail.com>
2022-10-25 23:45:32 +0430

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
+
+  get "user/:user_id/posts", to: "posts#index"
+  get "user/:user_is/posts/:id", to: "posts#show"
 end
