9a3204fbbea045f67fc1769b2c645f3d1d1649fb
shaqayq <shaqayq.darwazi@gmail.com>
2022-10-26 19:48:37 +0430

    Add controller RSpec

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,3 +1,42 @@
 require 'rails_helper'
-RSpec.describe 'Posts', type: :request do
+RSpec.describe 'Posts Controller', type: :request do
+
+    describe "GET /index" do
+        before do
+            get '/users/1/posts'
+        end
+       
+        it 'return correct response status' do
+            expect(response).to have_http_status(:ok)
+          end
+
+        it "correct template was rendere" do 
+            expect(response).to render_template(:index)
+        end
+
+        it "response body includes correct placeholder text" do
+            expect(response.body).to include("Post data will load")
+        end
+
+    end
+
+    describe "GET /show" do
+        before do
+            get '/users/1/posts/1'
+        end
+       
+        it 'return correct response status' do
+            expect(response).to have_http_status(:ok)
+          end
+
+        it "correct template was rendere" do 
+            expect(response).to render_template(:show)
+        end
+
+        it "response body includes correct placeholder text" do
+            expect(response.body).to include("Post details will display")
+        end
+
+    end
+    
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,3 +1,42 @@
 require 'rails_helper'
-RSpec.describe 'Users', type: :request do
+RSpec.describe 'Users Controller', type: :request do
+
+    describe "GET /index" do
+        before do
+            get '/'
+        end
+       
+        it 'return correct response status' do
+            expect(response).to have_http_status(:ok)
+          end
+
+        it "correct template was rendere" do 
+            expect(response).to render_template(:index)
+        end
+
+        it "response body includes correct placeholder text" do
+            expect(response.body).to include("user data will display")
+        end
+
+    end
+
+    describe "GET /show" do
+        before do
+            get '/users/1'
+        end
+       
+        it 'return correct response status' do
+            expect(response).to have_http_status(:ok)
+          end
+
+        it "correct template was rendere" do 
+            expect(response).to render_template(:show)
+        end
+
+        it "response body includes correct placeholder text" do
+            expect(response.body).to include("User details will display")
+        end
+
+    end
+  
 end
