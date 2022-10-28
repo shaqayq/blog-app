14e121eb8bdf7eb86e2431150339306719831264
shaqayq <shaqayq.darwazi@gmail.com>
2022-10-28 12:54:05 +0430

    Add views

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,15 +5,15 @@ class Post < ApplicationRecord
     after_save :user_counters
 
     validates :title , presence: true , length: {maximum: 250}
-    validates :comments_counter, :likes_counter, presence: true,
-                                               numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    # validates :comments_counter, :likes_counter, presence: true,
+    #                                            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
    
     
    def user_counters
     user.increment!(:post_counter)
    end 
     
-    def self.five_most_recent_post
-        order(created_at: :asc).limit(5)
+    def five_most_recent_comment
+       comments.order(created_at: :asc).limit(5)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
     has_many :comments 
   
     validates :name , presence: true
-    validates :post_counter , numericality: {only_integer: true, greater_than_or_equal_to: 0}
+    # validates :post_counter , numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
-    scope :recent_post, -> {order(created_at: :asc).limit(3)}
+    def recent_post
+        posts.order(created_at: :desc).limit(3)
+    end
 end
