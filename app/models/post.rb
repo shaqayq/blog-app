7156ec2a69b9c101dc0012f11817e432e1a1e8df
shaqayq <shaqayq.darwazi@gmail.com>
2022-10-28 13:01:18 +0430

    change views

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ApplicationRecord
     after_save :user_counters
 
     validates :title , presence: true , length: {maximum: 250}
-    # validates :comments_counter, :likes_counter, presence: true,
-    #                                            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :comments_counter, :likes_counter, presence: true,
+                                               numericality: { only_integer: true, greater_than_or_equal_to: 0 }
    
     
    def user_counters
